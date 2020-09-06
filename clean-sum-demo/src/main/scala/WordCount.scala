5f527c9fe74213c881c3d1dc6da1f6ea17264a4d
bayuedekui <468360786@qq.com>
2020-09-06 19:28:21 +0800

    改stanlone模式为local模式

--- clean-sum-demo/src/main/scala/WordCount.scala
+++ clean-sum-demo/src/main/scala/WordCount.scala
@@ -5,11 +5,11 @@ import org.apache.spark.sql.SparkSession
 object WordCount {
   def main(args: Array[String]): Unit = {
     //local模式
-    //val conf = new SparkConf().setAppName("WC").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("WC").setMaster("local[2]")
 
     //集群模式
-    val conf = new SparkConf().setAppName("WC").setMaster("spark://192.168.1.111:7077")
-    conf.setJars(List("D:\\EEEEEEEEEEEEEEEEEEEEEEEEEE\\111.txt"))
+//    val conf = new SparkConf().setAppName("WC").setMaster("spark://192.168.1.111:7077")
+//    conf.setJars(List("D:\EEEEEEEEEEEEEEEEEEEEEEEEEE\sparkcleansum\clean-sum-demo\target\clean-sum-demo-1.0-SNAPSHOT.jar"))
 
 
     val sc = SparkSession.builder().config(conf).getOrCreate().sparkContext
