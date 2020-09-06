e981f018fdb90125c8663402ecafdce8b749d8d8
bayuedekui <468360786@qq.com>
2020-09-06 11:51:49 +0800

    spark clean sum demo

---
+++ clean-sum-demo/src/main/scala/WordCount.scala
@@ -0,0 +1,25 @@
+import org.apache.spark
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //local模式
+    //val conf = new SparkConf().setAppName("WC").setMaster("local[2]")
+
+    //集群模式
+    val conf = new SparkConf().setAppName("WC").setMaster("spark://192.168.1.111:7077")
+    conf.setJars(List("D:\\EEEEEEEEEEEEEEEEEEEEEEEEEE\\111.txt"))
+
+
+    val sc = SparkSession.builder().config(conf).getOrCreate().sparkContext
+    val file = sc.textFile("D:\\\\EEEEEEEEEEEEEEEEEEEEEEEEEE\\\\111.txt")
+
+    val wc = file.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    val res = wc.collect()
+    for (r <- res) {
+      print(r)
+    }
+  }
+
+}
